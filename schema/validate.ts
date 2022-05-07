102a5e9cf190d01320a6a36080b39c42b037c3c1
Mark Hanna <cipscis+github@gmail.com>
2022-05-07 23:53:30 +1200

    Moved the rest of the policy validation code into validate.ts

--- schema/validate.ts
+++ schema/validate.ts
@@ -1,46 +1,96 @@
 import Ajv from 'ajv';
 import addFormats from 'ajv-formats';
 
+import { readdir, readFile } from 'fs/promises';
+import type { Dirent } from 'fs';
+
 import type { Policy } from './Policy.js';
+import type { PolicyVersionFile } from './PolicyVersionFile.js';
 
-const ajv = new Ajv();
+export const validatePolicy = await (async () => {
+	const ajv = new Ajv();
 
-// `ajv` doesn't understand complex formats by default,
-// they need to be added using `ajv-formats`
-addFormats(ajv, ['date', 'uri', 'uri-reference']);
+	// `ajv` doesn't understand complex formats by default,
+	// they need to be added using `ajv-formats`
+	addFormats(ajv, ['date', 'uri', 'uri-reference']);
 
-// `ajv` needs to know about every schema,
-// so load and add them all before creating the `ValidateFunction`
-const schemaNames = [
-	'policy',
-	'policy-version',
-	'policy-version-file',
-	'policy-version-duration',
-	'date-partial',
-	'provenance',
-	'accessibility',
-	'accessibility-feature',
-	'notice',
-	'licence',
-	'oia-request',
-	'oia-withholdings',
-	'oia-withholding',
-].map((name) => `${name}.schema.json`);
+	// `ajv` needs to know about every schema,
+	// so read the directory to automatically find them all, then
+	// load and add them all before creating the `ValidateFunction`
 
-const schemaPromises = schemaNames.map(async (name) => {
-	return (await import(`./${name}`, {
-		assert: { type: 'json' },
-	})).default;
-});
+	const schemaDir = await readdir('./schema');
+	const schemaFileNamePattern = /\.schema\.json$/;
+	const schemaFileNames = schemaDir.filter((fileName) => schemaFileNamePattern.test(fileName));
 
-const schemas = await Promise.all(schemaPromises);
+	const schemaPromises = schemaFileNames.map(async (name) => {
+		return (await import(`./${name}`, {
+			assert: { type: 'json' },
+		})).default;
+	});
 
-for (const [i, schema] of schemas.entries()) {
-	const name = schemaNames[i];
+	const schemas = await Promise.all(schemaPromises);
+	const policySchemaIndex = schemaFileNames.indexOf('policy.schema.json');
+	if (policySchemaIndex === -1) {
+		throw new Error('ERROR: Could not find policy schema file');
+	}
+	const policySchema = schemas[policySchemaIndex];
 
-	ajv.addSchema(schema, name);
+	for (const [i, schema] of schemas.entries()) {
+		const name = schemaFileNames[i];
+
+		ajv.addSchema(schema, name);
+	}
+
+	return ajv.compile<Policy>(policySchema);
+})();
+
+function getFileSizeValidator(dirName: string) {
+	return async function validateFileSize(file: PolicyVersionFile) {
+		const filePath = `${dirName}/${file.path}`;
+		const handle = await readFile(filePath);
+
+		if (file.size !== handle.byteLength) {
+			console.warn(`WARNING: Incorrect file size for ${filePath}. Was ${file.size}, should be ${handle.byteLength}`);
+			file.size = handle.byteLength;
+		}
+	}
 }
 
-const validatePolicy = ajv.compile<Policy>(schemas[0]);
+async function validateFileSizes(dirName: string, policy: Policy) {
+	const validateFileSize = getFileSizeValidator(dirName);
 
-export { validatePolicy };
+	const filePromises: Promise<void>[] = [];
+	for (const version of policy.versions) {
+		filePromises.push(...version.files.map(validateFileSize));
+	}
+
+	return Promise.all(filePromises);
+}
+
+export async function checkPolicyDir(entry: Dirent, directory: Record<string, unknown>) {
+	const dirName = `./policies/${entry.name}`;
+	const dir = await readdir(dirName);
+
+	if (dir.includes('metadata.json') === false) {
+		console.warn(`WARNING: No metadata.json file found for ${entry.name}`);
+		return;
+	}
+
+	const policy: unknown = (
+		await import(`../policies/${entry.name}/metadata.json`, {
+			assert: {
+				type: 'json',
+			},
+		})
+	).default;
+
+	const valid = validatePolicy(policy);
+	if (!valid) {
+		console.error(validatePolicy.errors);
+		throw new TypeError(`ERROR: Cannot build site due to invalid metadata in ${entry.name}`);
+	}
+
+	await validateFileSizes(dirName, policy);
+
+	directory[entry.name] = policy;
+}
--- webpack.config.ts
+++ webpack.config.ts
@@ -15,12 +15,9 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { htmlWebpackPluginTemplateCustomizer as TemplateCustomizer } from 'template-ejs-loader';
 
-import { readdir, readFile } from 'fs/promises';
-import type { Dirent } from 'fs';
+import { readdir } from 'fs/promises';
 
-import { validatePolicy } from './schema/validate.js';
-import type { PolicyVersionFile } from './schema/PolicyVersionFile.js';
-import type { Policy } from './schema/Policy.js';
+import { checkPolicyDir } from './schema/validate.js';
 
 const entryPath = './assets';
 const distPath = path.resolve(__dirname, '../dist');
@@ -82,54 +79,6 @@ switch (process.env.MODE) {
 		break;
 }
 
-function getFileSizeValidator(dirName: string) {
-	return async function validateFileSize(file: PolicyVersionFile) {
-		const filePath = `${dirName}/${file.path}`;
-		const handle = await readFile(filePath);
-
-		if (file.size !== handle.byteLength) {
-			console.warn(`WARNING: Incorrect file size for ${filePath}. Was ${file.size}, should be ${handle.byteLength}`);
-			file.size = handle.byteLength;
-		}
-	}
-}
-
-async function validateFileSizes(dirName, policy: Policy) {
-	const validateFileSize = getFileSizeValidator(dirName);
-
-	const filePromises: Promise<void>[] = [];
-	for (const version of policy.versions) {
-		filePromises.push(...version.files.map(validateFileSize));
-	}
-
-	return Promise.all(filePromises);
-}
-
-async function checkPolicy(entry: Dirent, directory: Record<string, unknown>) {
-	const dirName = `./policies/${entry.name}`;
-	const dir = await readdir(dirName);
-
-	if (dir.indexOf('metadata.json') === -1) return;
-
-	const data: unknown = (
-		await import(`./policies/${entry.name}/metadata.json`, {
-			assert: {
-				type: 'json',
-			},
-		})
-	).default;
-
-	const valid = validatePolicy(data);
-	if (!valid) {
-		console.error(validatePolicy.errors);
-		throw new TypeError(`Cannot build site due to invalid metadata in ${entry.name}`);
-	}
-
-	await validateFileSizes(dirName, data);
-
-	directory[entry.name] = data;
-}
-
 const result = (async () => {
 	const dir = await readdir('./policies', {
 		withFileTypes: true,
@@ -138,11 +87,10 @@ const result = (async () => {
 	const promises: Promise<void>[] = [];
 	const directory: Record<string, unknown> = {};
 
-	for (let i = 0, l = dir.length; i < l; ++i) {
-		const entry = dir[i];
+	for (const entry of dir) {
 		if (!entry.isDirectory()) continue;
 
-		promises.push(checkPolicy(entry, directory));
+		promises.push(checkPolicyDir(entry, directory));
 	}
 
 	await Promise.all(promises);
