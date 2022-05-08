dddfc6deb4e3622900243d788277c3cbcf464a7c
Brooke Hart <brooke+github@curly.kiwi>
2022-05-08 19:09:35 +1200

    Added colour scheme toggle to header

--- assets/js/main.ts
+++ assets/js/main.ts
@@ -1,4 +1,6 @@
 // This line is required for Webpack to find the CSS entry point without generating an empty JS file
 import '../css/style.scss';
 
+import './colour-scheme/colour-scheme.js';
+
 console.log('HERER');
