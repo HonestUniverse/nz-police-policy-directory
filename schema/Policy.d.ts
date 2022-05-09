f16155e8bee8230120da9dc875fc1780eee84975
Brooke Hart <brooke+github@curly.kiwi>
2022-05-09 21:47:42 +1200

    Added Speed Detection Equipment Operator's Manual Version 1, and updated schema and type definitions to include the "Equipment Operator's Manual" type

--- schema/Policy.d.ts
+++ schema/Policy.d.ts
@@ -5,7 +5,8 @@ declare enum PolicyType {
 	GENERAL_INSTRUCTIONS = 'General Instructions',
 	COMMISSIONERS_CIRCULAR = 'Commissioner\'s Circular',
 	CODE_OF_CONDUCT = 'Code of Conduct',
-	POLICE_MANUAL_CHAPTER = 'PoliceManualChapter',
+	POLICE_MANUAL_CHAPTER = 'Police Manual chapter',
+	EQUIPMENT_OPERATORS_MANUAL = 'Equipment Operator\'s Manual',
 }
 
 export type Policy = {
