e922b0113fcf2d84fd0e394a284ce6f4944c74d9
Brooke Hart <brooke+github@curly.kiwi>
2022-05-14 13:11:30 +1200

    Refactored build steps so it's easier to add new policy- or directory-specific build steps

---
+++ build/policy-build-steps.ts
@@ -0,0 +1,72 @@
+import type { PolicyBuildStep } from './BuildStep.js';
+
+import CopyPlugin from 'copy-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { htmlWebpackPluginTemplateCustomizer as TemplateCustomizer } from 'template-ejs-loader';
+
+import { toUrlSegment } from './util/to-url-segment.js';
+
+import * as paths from './build-paths.js';
+
+/**
+ * Build steps for a particular policy.
+ */
+export const policyBuildSteps: Record<string, PolicyBuildStep> = {
+	/**
+	 * Copy the metadata file for a policy to its destination
+	 */
+	copyMetadata(src, dst) {
+		return [new CopyPlugin({
+			patterns: [{ from: `${src}/metadata.json`, to: `${dst}/metadata.json` }],
+		})];
+	},
+
+	/**
+	 * Copy all files for a policy to their destinations
+	 */
+	copyFiles(src, dst, policy) {
+		const plugins: CopyPlugin[] = [];
+
+		for (const version of policy.versions) {
+			const versionName = version.name;
+			const versionUrl = toUrlSegment(versionName);
+
+			for (const file of version.files) {
+				const fileSrcPathAndName = `${src}/${file.path}`;
+				const fileName = file.path.replace(/.*\//, '');
+
+				const fileDstPath = toUrlSegment(versionUrl);
+				const fileDstPathAndName = `${dst}/${fileDstPath}/${fileName}`;
+
+				plugins.push(
+					new CopyPlugin({
+						patterns: [{ from: fileSrcPathAndName, to: fileDstPathAndName }],
+					})
+				);
+
+				// Update file.path to ensure the build HTML points to the correct place
+				file.path = `${fileDstPath}/${fileName}`;
+			}
+		}
+
+		return plugins;
+	},
+
+	/**
+	 * Generate the HTML for a Policy page
+	 */
+	createPolicyPage(src, dst, policy) {
+		return [new HtmlWebpackPlugin({
+			filename: `${dst}/index.html`,
+			template: TemplateCustomizer({
+				templatePath: `${paths.templates}/policy.ejs`,
+				templateEjsLoaderOption: {
+					data: { policy },
+				},
+			}),
+			chunks: ['main', 'priority'],
+		})];
+	},
+
+	// TODO: Also generate a page for each version of the policy
+};
