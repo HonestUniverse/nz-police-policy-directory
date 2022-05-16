fe964c5d1fa8949f875d82679558b12e6bb9c315
Mark Hanna <cipscis+github@gmail.com>
2022-05-16 21:59:28 +1200

    Fixed typo

--- schema/Accessibility.d.ts
+++ schema/Accessibility.d.ts
@@ -2,7 +2,7 @@ import { AccessibilityFeature } from './AccessibilityFeature.js';
 
 export type Accessibility = {
 	'text-based': AccessibilityFeature,
-	semanticis: AccessibilityFeature,
+	semantics: AccessibilityFeature,
 	'alt text': AccessibilityFeature,
 	unwatermarked: AccessibilityFeature,
 };
