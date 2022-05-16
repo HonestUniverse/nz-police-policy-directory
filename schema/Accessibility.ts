1f895bd7de9cd6948a5f97158b07d1c1cc048cf3
Brooke Hart <mark.hanna@gettimely.com>
2022-05-16 22:35:56 +1200

    Schema v3.0.2 - Added accessibility rating

--- schema/Accessibility.ts
+++ schema/Accessibility.ts
@@ -1,6 +1,15 @@
 import { AccessibilityFeature } from './AccessibilityFeature.js';
 
+export enum AccessibilityRating {
+	PASS = 'Pass',
+	PARTIAL = 'Partial',
+	FAIL = 'Fail',
+	UNDETERMINED = 'Undetermined',
+}
+
 export type Accessibility = {
+	rating?: AccessibilityRating,
+
 	'text-based': AccessibilityFeature,
 	semantics: AccessibilityFeature,
 	'alt text': AccessibilityFeature,
