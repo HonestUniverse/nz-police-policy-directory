4508345aa4546c1c096f64d1fb3b3cf08dff18a8
Brooke Hart <brooke+github@curly.kiwi>
2022-05-18 20:25:45 +1200

    Create a specific directory JSON file

--- build/directory-build-steps.ts
+++ build/directory-build-steps.ts
@@ -1,15 +1,25 @@
 import type { DirectoryBuildStep } from './BuildStep.js';
 
+import GenerateJsonPlugin from 'generate-json-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { htmlWebpackPluginTemplateCustomizer as TemplateCustomizer } from 'template-ejs-loader';
 
 import * as paths from './util/paths.js';
 
 export const directoryBuildSteps: Record<string, DirectoryBuildStep> = {
+	createDirectoryMetadata(src, dst, directory) {
+		return [new GenerateJsonPlugin(
+			`${dst}.json`,
+			directory,
+			null,
+			'\t'
+		)];
+	},
+
 	/**
 	 * Generate the HTML for the Directory page
 	 */
-	buildDirectoryPage(src, dst, directory) {
+	createDirectoryPage(src, dst, directory) {
 		return [
 			new HtmlWebpackPlugin({
 				filename: `${dst}/index.html`,
@@ -27,5 +37,5 @@ export const directoryBuildSteps: Record<string, DirectoryBuildStep> = {
 				chunks: ['priority', 'main', 'enhancements'],
 			}),
 		];
-	}
+	},
 };
