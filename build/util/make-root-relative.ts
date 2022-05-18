80bc5285c742247a3cfbe5f39c224bee97fad8d3
Mark Hanna <cipscis+github@gmail.com>
2022-05-18 21:38:31 +1200

    Removed unnecessary step from `makeRootRelative`

--- build/util/make-root-relative.ts
+++ build/util/make-root-relative.ts
@@ -4,10 +4,8 @@ import path from 'path';
  * Turn a relative path into a root-relative path
  */
 export function makeRootRelative(relativePath: string): string {
-	// Create absolute path
-	const systemAbs = path.resolve('.', relativePath);
-	// Convert it to a relative path, from the current directory
-	const systemRel = path.relative('.', systemAbs);
+	// Create a relative path from the root to the current directory
+	const systemRel = path.relative('.', relativePath);
 	// Path may use "\" depending on the system it ran on, replace with web-safe "/"
 	const webRel = systemRel.replace(/\\/g, '/');
 	// Make root-relative
