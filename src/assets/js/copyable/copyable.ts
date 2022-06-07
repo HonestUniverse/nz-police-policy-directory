005c750cb72783be260d4b0d5a6eed28edd783a1
Mark Hanna <cipscis+github@gmail.com>
2022-06-07 21:51:09 +1200

    Made copy button work

---
+++ src/assets/js/copyable/copyable.ts
@@ -0,0 +1,59 @@
+enum Selector {
+	WRAPPER = '.js-copyable',
+	BUTTON = '.js-copyable__button',
+	CONTENT = '.js-copyable__content',
+}
+
+function init() {
+	initEvents();
+}
+
+/**
+ * Bind all necessary event listeners
+ */
+function initEvents() {
+	const $buttons = document.querySelectorAll<HTMLElement>(Selector.BUTTON);
+	$buttons.forEach(($button) => {
+		$button.addEventListener('click', copyEvent);
+	});
+};
+
+function copyEvent(this: HTMLElement, e: MouseEvent) {
+	const $button = this;
+	const $wrapper = $button.closest(Selector.WRAPPER);
+	const $content = $wrapper?.querySelector<HTMLElement>(Selector.CONTENT);
+
+	if ($content) {
+		copy($content);
+	}
+};
+
+/**
+ * Copy the text content of an element to the clipboard
+ */
+async function copy($element: HTMLElement): Promise<void> {
+	if (navigator.clipboard) {
+		const content = $element.innerText;
+		const contentBlob = new Blob([content], { type: 'text/plain' });
+		const contentItem = new ClipboardItem({ 'text/plain': contentBlob });
+		await navigator.clipboard.write([contentItem]);
+	} else {
+		const range = document.createRange();
+		range.selectNode($element);
+
+		const selection = window.getSelection();
+		if (!selection) {
+			// This can only happen in weird circumstances, should never show up in practice
+			return;
+		}
+
+		selection.removeAllRanges();
+		selection.addRange(range);
+
+		document.execCommand('copy');
+
+		selection.removeAllRanges();
+	}
+}
+
+init();
