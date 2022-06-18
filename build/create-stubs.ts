c2b9dfc82e836f112a4f855a0818734e73a0e278
Mark Hanna <cipscis+github@gmail.com>
2022-06-18 15:13:05 +1200

    Updated stub creation framework, and added code to create stubs from IR-01-22-11586 response. Still some review necessary

--- build/create-stubs.ts
+++ build/create-stubs.ts
@@ -5,42 +5,72 @@ import type { Policy } from '../schema/Policy.js';
 import { toUrlSegment } from './util/to-url-segment.js';
 import * as paths from './util/paths.js';
 
-type StubPolicy = Omit<Policy, 'name' | 'versions'>;
+export type StubPolicyWith<TProps extends keyof Policy> = Omit<Policy, Exclude<keyof Policy, TProps>>
+export type StubPolicyWithout<TProps extends keyof Policy> = Omit<Policy, TProps>
 
 const policiesSrc = paths.policies;
 
 /**
- * Create a stub for each document in a given list of names
+ * Some policy names are listed with the word "Part" abbreviated to "Pt".
  */
-export async function createStubs(names: Policy['name'][], stubPolicy: StubPolicy) {
+function transformName(name: string): string {
+	if (/Pt\s*\d+/.test(name)) {
+		name = name.replace(/Pt\s*(\d+)/, 'Part $1');
+	}
+
+	return name;
+}
+
+/**
+ * TypeScript doesn't automatically narrow a type based on `in`, but a custom typeguard can do this
+ */
+function hasNameProp(obj: any): obj is { name: string } {
+	return typeof obj?.name === 'string';
+}
+
+/**
+ * Create a stub for each document in a given list of stubs with a shared base
+ */
+export async function createStubs<TProps extends keyof Policy>(stubsOriginal: StubPolicyWith<TProps>[], baseOriginal: StubPolicyWithout<TProps>) {
 	const promises: Promise<unknown>[] = [];
 
-	for (let name of names) {
-		if (/Pt\s*\d+/.test(name)) {
-			name = name.replace(/Pt\s*(\d+)/, 'Part $1');
-		}
+	// Make deep copies
+	const base = JSON.parse(JSON.stringify(baseOriginal)) as typeof baseOriginal;
+	const stubs = stubsOriginal.map((stub) => JSON.parse(JSON.stringify(stub)) as typeof stubsOriginal[number]);
 
-		const chapter: Policy = {
-			name,
-			versions: [],
-			...stubPolicy,
-		};
+	if (hasNameProp(base)) {
+		base.name = transformName(base.name);
+	} else {
+		stubs.forEach((stub) => {
+			if (hasNameProp(stub)) {
+				stub.name = transformName(stub.name);
+			}
+		});
+	}
 
-		const chapterDir = `${policiesSrc}/${toUrlSegment(name)}`;
+	for (const stub of stubs) {
+		// TypeScript isn't smart enough to infer this type, so a type assertion is necesary
+		// It's safe though, since PolicyStubWith<TProps> & PolicyStubWithout<TProps> is, by definition, equivalent to Policy
+		const policy: Policy = {
+			...stub,
+			...base,
+		} as Policy;
+
+		const chapterDir = `${policiesSrc}/${toUrlSegment(policy.name)}`;
 		const chapterMetadataLocation = `${chapterDir}/metadata.json`;
 
 		promises.push(new Promise<void>((resolve, reject) => {
 			// Try to read the file. If it doesn't exist, create a stub page
 			readFile(chapterMetadataLocation).catch(() => {
 				mkdir(chapterDir, { recursive: true }).then(() => {
-					writeFile(chapterMetadataLocation, JSON.stringify(chapter, null, '\t'))
+					writeFile(chapterMetadataLocation, JSON.stringify(policy, null, '\t'))
 				})
 				.then(() => {
-					console.log(`INFO: Created stub for ${name}`);
+					console.log(`INFO: Created stub for ${policy.name}`);
 					resolve();
 				})
 				.catch((reason) => {
-					console.error(`ERROR: Failed to created stub for ${name}`);
+					console.error(`ERROR: Failed to created stub for ${policy.name}`);
 					console.error(reason);
 					reject(reason);
 				});
